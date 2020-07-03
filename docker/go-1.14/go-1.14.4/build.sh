17ce7709e157cc9c85d3eb476f99faad0c34e937
Hank Shen <swhbox@foxmail.com>
2020-07-03 15:07:47 +0800

    update

--- docker/go-1.14/go-1.14.4/build.sh
+++ docker/go-1.14/go-1.14.4/build.sh
@@ -3,3 +3,7 @@ docker build -t admpub/xgo:1.14.4 .
 #推送到仓库：docker push admpub/xgo:1.14.4
 #修改名称：docker tag IMAGEID(镜像id) REPOSITORY:TAG（仓库：标签）
 #提交修改：docker commit -m "提交说明" -a "用户描述" 容器ID admpub/xgo:2.0.0
+# ------------- 更新到latest ---------------
+#运行命令获取容器ID：docker run -it --entrypoint /bin/bash  admpub/xgo:1.14.4
+#docker commit -m "update" 容器ID admpub/xgo:latest
+#docker push admpub/xgo:latest
