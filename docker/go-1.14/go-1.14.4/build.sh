0eabfbc5eaa0a9ae5b399e45c58107352d2bd7f5
Hank Shen <swhbox@foxmail.com>
2020-12-17 14:17:05 +0800

    Added go1.15.6

--- docker/go-1.14/go-1.14.4/build.sh
+++
@@ -1,9 +0,0 @@
-docker build -t admpub/xgo:1.14.4 .
-#docker build -t admpub/xgo:1.14.4 -f ./go-1.14/go-1.14.4/Dockerfile .
-#推送到仓库：docker push admpub/xgo:1.14.4
-#修改名称：docker tag IMAGEID(镜像id) REPOSITORY:TAG（仓库：标签）
-#提交修改：docker commit -m "提交说明" -a "用户描述" 容器ID admpub/xgo:2.0.0
-# ------------- 更新到latest ---------------
-#运行命令获取容器ID：docker run -it --entrypoint /bin/bash  admpub/xgo:1.14.4
-#docker commit -m "update" 容器ID admpub/xgo:latest
-#docker push admpub/xgo:latest
---
+++ docker/go-latest/build.sh
@@ -0,0 +1,11 @@
+export GO_LATEST_VERSION=1.15.6
+export GO_DOCKERFILE_DIR=go-1.15
+docker build -t admpub/xgo:${GO_LATEST_VERSION} .
+#docker build -t admpub/xgo:${GO_LATEST_VERSION} -f ./${GO_DOCKERFILE_DIR}/go-${GO_LATEST_VERSION}/Dockerfile .
+#推送到仓库：docker push admpub/xgo:${GO_LATEST_VERSION}
+#修改名称：docker tag IMAGEID(镜像id) REPOSITORY:TAG（仓库：标签）
+#提交修改：docker commit -m "提交说明" -a "用户描述" 容器ID admpub/xgo:2.0.0
+# ------------- 更新到latest ---------------
+#运行命令获取容器ID：docker run -it --entrypoint /bin/bash  admpub/xgo:${GO_LATEST_VERSION}
+#docker commit -m "update" 容器ID admpub/xgo:latest
+#docker push admpub/xgo:latest
